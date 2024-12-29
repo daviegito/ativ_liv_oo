8829032be27cb66ab3bcc837294fc6db20c2a93b
Davi do Egito <daviegitocoelho@gmail.com>
2024-12-29 15:58:01 -0300

    fix: removidos comentários do arquivo principal - a serem adicionados no README

--- atividade_livre/atividade_livre.py
+++ atividade_livre/atividade_livre.py
@@ -1,6 +1,3 @@
-"""Este projeto será sobre a criação de um programa para ajudar pessoas a otimizarem a escolha de matérias
-durante a graduação (por enquanto, só Software) levando em conta pré-requisitos e cadeias."""
-
 import json
 
 #Classes 
@@ -175,27 +172,3 @@ while opcao != 4:
     except ValueError:
         opcao = -1 #isto evita um problema do loop parar em uma opção e não voltar ao menu corretamente
 print("\nPrograma encerrado")
- 
-
-
-
-#Para uso posterior, caso além das 5 matérias, ele possa citar as cadeias também - a ser avaliado ainda:
-"""
-*** Matérias de Engenharia de Software ***
-Cálculo 1[3]: Cálculo 2; Probabilidade e Estatística Aplicada à Engenharia; Métodos Numéricos para Engenharia; 
-Algoritmos e Programação de Dados[3]: Orientação a Objetos; Estrutura de Dados; Projeto e Análise de Algoritmos;
-Orientação a Objetos[4]: Paradigmas de Programação; Métodos de Desenvolvimento de Software; Projeto Integrador de Engenharia 1; Projeto Integrador de Engenharia 2; 
-Métodos de Desenvolvimento de Software[9]: Interação Humano Computador; Qualidade de Software 1; Requisitos de Software; Arquitetura e Desenho de Software; Técnicas de Programação em Plataformas Emergentes; Engenharia de Produto de Software; Testes de Software; Gerência de Configuração e Evolução de Software; Projeto Integrador de Engenharia 2; 
-Estrutura de Dados[5]: Compiladores 1; Paradigmas de Programação; Estrutura de Dados 2; Programação para Sistemas Paralelos e Distribuídos; Projeto e Análise de Algoritmos;
-Desenho Industrial Assistido por Computador[1]
-Engenharia e Ambiente[1]
-Estágio Supervisionado[1]
-Trabalho de Conclusão de Curso 1[2]: Trabalho de Conclusão de Curso 2;
-Física 1[1]
-Física 1 Experimental[1]
-Humanidades e Cidadania[1]
-Introdução à Engenharia[1]
-Engenharia Econômica[2]: Gestão da Produção e Qualidade; Qualidade de Software 1;
-Matemática Discreta 1[3]: Matemática Discreta 2; Sistemas de Banco de Dados 1; Sistemas de Banco de Dados 2;
-Introdução à Álgebra Linear[6]: TED/PED1; Fundamentos de Arquitetura de Computadores; Fundamentos de Sistemas Operacionais; Fundamentos de Redes de Computadores; Programação para Sistemas Paralelos e Distribuídos [sequência acaba aqui]; Fundamentos de Sistemas Embarcados.
-"""
